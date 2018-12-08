5fd66fbfd1a7804b196801773e15a64790b85a45
MikeHowellTransferenceLtdCoUk <mike.howell@transferenceltd.co.uk>
2018-12-08 12:20:24 +0000

    Add project files.

---
+++ ShoppingListTask/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingListTask
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
